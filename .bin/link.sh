65708ef8a0f612907c2a0e81da0b163dd118dfe8
Brandon Scott-Hill <bran.scotthill@gmail.com>
2022-06-12 21:15:21 +1200

    Move util to .bin

---
+++ .bin/link.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+ln -f ~/dotfiles/.config/* ~/.config
+ln -f ~/dotfiles/xsessions/* /usr/share/xsessions
--- util/link.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-ln ~/dotfiles/.config/dwm-flexipatch/patches.def.h ~/.config/dwm-flexipatch/patches.def.h
