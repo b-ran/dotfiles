db4e11f6c612ba67133524ef057843d16fece567
Brandon Scott-Hill <bran.scotthill@gmail.com>
2022-06-13 23:29:43 +1200

    Add dwm-flxipatch config files

--- .bin/link.sh
+++ .bin/link.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-cp -rsf ~/dotfiles/.config/*/ ~/.config/*/
-sudo mkdir -p /usr/share/xsessions && cp -rs ~/dotfiles/xsessions/* /usr/share/xsessions
+cp -rsfu ~/dotfiles/.config/* ~/.config/
+sudo mkdir -p /usr/share/xsessions && cp -rsu ~/dotfiles/xsessions/* /usr/share/xsessions
