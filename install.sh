0bbe606bd8f00e62dfce7ca23ea692e9cfbc6be5
Brandon Scott-Hill <bran.scotthill@gmail.com>
2022-06-19 23:11:46 +1200

    Add dwm-blocks

--- install.sh
+++ install.sh
@@ -1,22 +1,25 @@
-sudo pacman -S base-devel xorg xorg-xinit xorg-xrdb picom archlinux-keyring alacritty sxhkd feh --noconfirm
+sudo pacman -S --needed base-devel xorg xorg-xinit xorg-xrdb picom archlinux-keyring alacritty sxhkd feh ttf-iosevka-nerd --noconfirm
 
 # Install aur helper -- paru
 git clone https://aur.archlinux.org/paru.git
 cd paru && makepkg -si --noconfirm
 cd .. && rm -rf paru
 
-# Install window manager
+# Install window manager - dwmflexipatch
 mkdir .config
 git clone https://github.com/bakkeby/dwm-flexipatch.git .config/dwm
 cd ~/.config/dwm && sudo make install
+
+# Install status bar - dwm blocks
+git clone https://github.com/torrinfail/dwmblocks.git .config/dwmblocks
+cd ~/.config/dwmblocks && sudo make install
 
 # Install display manager
 paru -S ly --noconfirm
 sudo systemctl enable ly.service
 
-# Install application launcher
-
-paru -S rofi --noconfirm
+# Install packages
+paru -S rofi nerd-fonts-complete tmux-git firefox --noconfirm
 
 # Copy dotfiles
 ~/dotfiles/.bin/copy.sh
