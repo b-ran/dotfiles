15eb8704f42d9e34d9227043c2658c1dd9db41d1
Brandon Scott-Hill <bran.scotthill@gmail.com>
2022-07-05 21:59:28 +1200

    Fixing dwmblocks

--- install.sh
+++ install.sh
@@ -7,9 +7,6 @@ cd .. && rm -rf paru
 
 # Install dwm
 mkdir .config
-git clone https://github.com/bakkeby/dwm-flexipatch.git ~/.config/dwm
-git clone https://github.com/torrinfail/dwmblocks.git ~/.config/dwmblocks
-
 ~/dotfiles/scripts/copy.sh
 cd ~/.config/dwm && sudo make install
 cd ~/.config/dwmblocks && sudo make install
@@ -19,10 +16,4 @@ paru -S ly --noconfirm
 sudo systemctl enable ly.service
 
 # Install packages
-paru -S rofi tmux-git firefox --noconfirm
-
-
-git clone https://aur.archlinux.org/nerd-fonts-complete.git
-cd nerd-fonts-complete
-wget -O nerd-fonts-2.1.0.tar.gz https://github.com/ryanoasis/nerd-fonts/archive/v2.1.0.tar.gz
-makepkg -sci BUILDDIR=.
+paru -S rofi tmux-git firefox nerd-fonts-complete pamixer --noconfirm
--- scripts/blocks/memory.sh
+++ scripts/blocks/memory.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 . ~/.config/themes/blocks
 
-printf "^b#1E222A^ ^c#7797b7^  "
-printf "^b#2E3440^ ^c#7797b7^ %s $(free -h | awk '/^Mem/ { print $3"/"$2 }' | sed s/i//g)"
+echo "^b#2E3440^^c#7797b7^   $(free -h | awk '/^Mem/ { print $3"/"$2 }' | sed s/i//g)"
