7ede64487c8cda2350560fedd6c3bd817f2f695e
Brandon Scott-Hill <bran.scotthill@gmail.com>
2022-07-06 23:35:06 +1200

    Fix block spacing

--- scripts/blocks/cpu.sh
+++ scripts/blocks/cpu.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 . ~/.config/themes/blocks
 
-echo "^b$bar^ ^c$cpu_ic_fg^^b$cpu_ic_bg^  ^c$cpu_fg^^b$cpu_bg^$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1"%"}') "
+echo "^b$bar^ ^c$cpu_ic_fg^^b$cpu_ic_bg^  ^c$cpu_fg^^b$cpu_bg^ $(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1"%"}') "
--- scripts/blocks/date.sh
+++ scripts/blocks/date.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 . ~/.config/themes/blocks
 
-echo "^b$bar^ ^c$date_ic_fg^^b$date_ic_bg^  ^c$date_fg^^b$date_bg^$(date '+%a, %d/%m') "
+echo "^b$bar^ ^c$date_ic_fg^^b$date_ic_bg^  ^c$date_fg^^b$date_bg^ $(date '+%a, %d/%m') "
