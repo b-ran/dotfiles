70498d6fb82e4154c157ce83910024879164f2e6
Brandon Scott-Hill <bran.scotthill@gmail.com>
2022-07-07 21:26:57 +1200

    Fix tag coloring been same as the bar

--- .config/themes/dwm.h
+++ .config/themes/dwm.h
@@ -1,19 +1,19 @@
 #define bar #1E222A
-#define unfocused_window_border #2E323A
-#define focused_window_border #61AFEF
+#define unfocused #2E323A
+#define focused #61AFEF
 #define unknown #FE8019
 #define unknown2 #FE1925
 
 
 dwm.normfgcolor                : unknown
 dwm.normbgcolor                : bar
-dwm.normbordercolor            : bar
-dwm.normfloatcolor             : unfocused_window_border
+dwm.normbordercolor            : unfocused
+dwm.normfloatcolor             : unfocused
 
 dwm.selfgcolor                 : unknown
-dwm.selbgcolor                 : focused_window_border
-dwm.selbordercolor             : focused_window_border
-dwm.selfloatcolor              : focused_window_border
+dwm.selbgcolor                 : focused
+dwm.selbordercolor             : focused
+dwm.selfloatcolor              : focused
 
 dwm.titlenormfgcolor           : unknown
 dwm.titlenormbgcolor           : bar
@@ -25,7 +25,7 @@ dwm.titleselbgcolor            : unknown
 dwm.titleselbordercolor        : unknown
 dwm.titleselfloatcolor         : unknown
 
-dwm.tagsnormfgcolor		       : bar
+dwm.tagsnormfgcolor            : bar
 dwm.tagsnormbgcolor            : bar
 dwm.tagsnormbordercolor        : unknown
 dwm.tagsnormfloatcolor         : unknown
@@ -40,10 +40,10 @@ dwm.hidselfgcolor              : unknown2
 dwm.hidnormbgcolor             : unknown2
 dwm.hidselbgcolor              : unknown2
 
-dwm.urgfgcolor				   : unknown
-dwm.urgbgcolor				   : unknown
-dwm.urgbordercolor			   : unknown
-dwm.urgfloatcolor			   : unknown
+dwm.urgfgcolor                 : unknown
+dwm.urgbgcolor                 : unknown
+dwm.urgbordercolor             : unknown
+dwm.urgfloatcolor              : unknown
 
 dwm.tags.fg1                   : #E06C75
 dwm.tags.fg2                   : #98C379
@@ -51,9 +51,6 @@ dwm.tags.fg3                   : #E5C07B
 dwm.tags.fg4                   : #61AFEF
 dwm.tags.fg5                   : #C678DD
 dwm.tags.fg6                   : #56B6C2
-dwm.tags.fg7                   : #D7424D
-dwm.tags.fg8                   : #7DB356
-dwm.tags.fg9                   : #ABB2BF
 dwm.tags.layoutfg              : bar
 
 dwm.tags.bg1                   : bar
@@ -62,7 +59,3 @@ dwm.tags.bg3                   : bar
 dwm.tags.bg4                   : bar
 dwm.tags.bg5                   : bar
 dwm.tags.bg6                   : bar
-dwm.tags.bg7                   : bar
-dwm.tags.bg8                   : bar
-dwm.tags.bg9                   : bar
-dwm.tags.layoutbg              : bar
