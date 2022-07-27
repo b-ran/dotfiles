829686c4c7b2f8ca1934374af861f14e1052a63b
Brandon Scott-Hill <bran.scotthill@gmail.com>
2022-07-27 16:29:14 +1200

    Add app install selection

--- apps.sh
+++ apps.sh
@@ -1,1 +1,32 @@
-paru -S coolero thunar firefox --noconfirm
+#!/bin/bash
+
+PS3='Packages sets - enter choice: '
+options=("Home" "Dev" "Games" "General" "Nvidia" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Home")
+            paru -S coolero discord betterdiscord-installer qbittorrent --noconfirm
+            ;;
+        "Dev")
+            paru -S keepassxc remmina freerdp docker pycharm-professional webstorm python slack-desktop gparted --noconfirm
+            sudo groupadd docker
+            sudo usermod -aG docker "$USER"
+            sudo systemctl enable docker.service
+            sudo systemctl enable containerd.service
+            ;;
+        "Games")
+            paru -S steam lutris-git bottles --noconfirm
+            ;;
+        "General")
+            paru -S firefox chromium spotify spotify-tui --noconfirm
+            ;;
+        "Nvidia")
+            paru -S nvidia nvidia-utils nvidia-settings --noconfirm
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
