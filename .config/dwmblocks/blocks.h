64f31b3e9d9efe23da26e7c06034fb2a4a32a98b
Brandon Scott-Hill <bran.scotthill@gmail.com>
2022-08-25 21:27:46 +1200

    Fixing dwmblcoks

--- .config/dwmblocks/blocks.h
+++ .config/dwmblocks/blocks.h
@@ -1,12 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "~/dotfiles/scripts/blocks/volume",      1,  0},
-	{"", "~/dotfiles/scripts/blocks/cpu",         3,  0},
-	{"", "~/dotfiles/scripts/blocks/memory",      3,  0},
-	{"", "~/dotfiles/scripts/blocks/storage",     3,  0},
-	{"", "~/dotfiles/scripts/blocks/time",        3,  0},
-	{"", "~/dotfiles/scripts/blocks/uptime",      30, 0},
+	{"", "~/dotfiles/scripts/blocks/volume.sh",      1,  0},
+	{"", "~/dotfiles/scripts/blocks/cpu.sh",         3,  0},
+	{"", "~/dotfiles/scripts/blocks/memory.sh",      3,  0},
+	{"", "~/dotfiles/scripts/blocks/storage.sh",     3,  0},
+	{"", "~/dotfiles/scripts/blocks/time.sh",        3,  0},
+	{"", "~/dotfiles/scripts/blocks/uptime.sh",      30, 0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
